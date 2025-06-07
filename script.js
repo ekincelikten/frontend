88e8b81f5e932a23f634823efe81dd909ac2c7f6
ekincelikten <ekinheisenberg@gmail.com>
2025-06-07 22:36:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+// Şu anda tüm JS kodları index.html içine gömülü durumda.
+// Bu dosya ileride kodları dışarı taşımak için ayrılmıştır.
+
+// Örnek başlangıç kodu:
+console.log("script.js yüklendi");
