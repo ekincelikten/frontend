5c40597a647f9c30649394d111094b779fc977fe
ekincelikten <ekinheisenberg@gmail.com>
2025-06-08 01:37:59 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-// Şu anda tüm JS kodları index.html içine gömülü durumda.
-// Bu dosya ileride kodları dışarı taşımak için ayrılmıştır.
-
-// Örnek başlangıç kodu:
-console.log("script.js yüklendi");
